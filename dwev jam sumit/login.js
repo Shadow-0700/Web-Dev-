4c41794b8048beedb7336ef18e247bb5e1fbc006
Sumit saini <isumitsaini18@gmail.com>
2025-02-03 16:58:50 +0530

    Delete dwev jam sumit directory

--- dwev jam sumit/login.js
+++
@@ -1,8 +0,0 @@
-function fun() {
-    var email=document.getElementById("username").value;
-    var pass=document.getElementById("password").value;
-    if(email=='sumit.18@gmail.com' && pass=='Sumit@13'){
-        alert("Succsefully Login!")
-      window.location.assign("profile.html");
-    }
-    }
